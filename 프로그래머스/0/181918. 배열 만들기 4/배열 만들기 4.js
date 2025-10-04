dcd387975f12c0f312ce3477f62d144bde688aa5
우기 <113000290+WooGi1020@users.noreply.github.com>
2025-10-04 16:10:59 +0900

    [level 0] Title: 배열 만들기 4, Time: 37.63 ms, Memory: 38.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
@@ -0,0 +1,19 @@
+function solution(arr) {
+    var stk = [];
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (stk.length === 0){
+            stk.push(arr[i]);
+        } else {
+            if (stk.length && stk.at(-1) < arr[i]) {
+                stk.push(arr[i]);
+            } else {
+                stk.pop();
+                i -= 1;
+            }
+        }
+
+    }
+    
+    return stk;
+}
