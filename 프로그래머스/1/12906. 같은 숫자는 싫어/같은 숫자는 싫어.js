c04f0205c95615d259bf5590a6eecd64e1c35492
우기 <113000290+WooGi1020@users.noreply.github.com>
2026-01-21 21:12:33 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 38.34 ms, Memory: 82.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,11 @@
+function solution(arr)
+{
+    let answer = [];
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (i > 0 && arr[i - 1] === arr[i]) continue;
+        answer.push(arr[i]);
+    }
+    
+    return answer;
+}
